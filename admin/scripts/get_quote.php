457045116ea1c760c1d2717adfdf6a31028558e5
Akahibe Kingsley <divine10646@gmail.com>
2021-02-10 12:16:21 +0100

    mail debugging

--- admin/scripts/get_quote.php
+++ admin/scripts/get_quote.php
@@ -95,14 +95,15 @@ function sendMail(){
 
   try {
     //Server settings
-    //$mail->SMTPDebug = 1;                      // Enable verbose debug output
+    $mail->SMTPDebug = 2;                      // Enable verbose debug output
     $mail->isSMTP();                                            // Send using SMTP
     $mail->Host       = 'mail.vendorcrest.com ';                    // Set the SMTP server to send through
     $mail->SMTPAuth   = true;                                   // Enable SMTP authentication
-    $mail->Username   = 'info@vendorcrest.com';                     // SMTP username
+    //$mail->Username   = 'info@vendorcrest.com';                     // SMTP username
+    $mail->Username   = 'rbx105.truehost.cloud';                     // SMTP username
     $mail->Password   = 'kingsley-et-diva';                               // SMTP password
     $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;         // Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` encouraged
-    $mail->Port       = 465;                                    // TCP port to connect to, use 465 for `PHPMailer::ENCRYPTION_SMTPS` above
+    $mail->Port       = 587;                                    // TCP port to connect to, use 465 for `PHPMailer::ENCRYPTION_SMTPS` above
 
     //Recipients
     $mail->setFrom('info@vendorcrest.com', 'Vendorcrest Digital');
