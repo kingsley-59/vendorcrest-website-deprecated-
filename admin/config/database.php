f0bbfc4c70a5b496c505bd4bb6a86f9ae3745d8c
Akahibe Kingsley <divine10646@gmail.com>
2021-05-18 09:19:22 +0100

    first publish

--- admin/config/database.php
+++ admin/config/database.php
@@ -1,13 +1,13 @@
 <?php
 $hostname     = "localhost"; // Enter Your Host Name
 
-// $username     = "vendorc1_kingsley";      // Enter Your Database user username
-// $password     = "kingsley-et-diva";          // Enter Your Database user Password
-// $databasename = "vendorc1_admin_panel"; // Enter Your database Name
+$username     = "vendorc1_kingsley";      // Enter Your Database user username
+$password     = "kingsley-et-diva";          // Enter Your Database user Password
+$databasename = "vendorc1_admin_panel"; // Enter Your database Name
 
-$username     = "root";      // Enter Your Database user username
-$password     = "";          // Enter Your Database user Password
-$databasename = "admin_panel"; // Enter Your database Name
+// $username     = "root";      // Enter Your Database user username
+// $password     = "";          // Enter Your Database user Password
+// $databasename = "admin_panel"; // Enter Your database Name
 
 $conn = new mysqli($hostname, $username, $password, $databasename);
 
